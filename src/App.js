a92cd51bcf9b3a7c7f40acea6b74f439baf9af32
Rishav Anand <rishva343@gmail.com>
2022-06-12 15:15:05 +0530

    Sorting Algorithms i.e; merge-sort-algorithm added for now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
-import SortingVisualizer from "./SortingVisualizer/SortingVisualizer";
+import SortingVisualizer from "./sortingVisualizer/SortingVisualizer";
 
 function App() {
   return (
     <div className="App">
-      <SortingVisualizer >
-      
-      </SortingVisualizer>
       <h1>Hello</h1>
+      <SortingVisualizer />
     </div>
   );
 }
--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./SortingVisualizer.css";
+import "../sortingAlgorithms/SortingAlgorithms"
 
 // Change this value for the speed of the animations.
 const ANIMATION_SPEED_MS = 1;
@@ -8,7 +9,7 @@ const ANIMATION_SPEED_MS = 1;
 const NUMBER_OF_ARRAY_BARS = 284;//Screen.width/4-100;
 
 // This is the main color of the array bars.
-const PRIMARY_COLOR = 'turquoise';
+const PRIMARY_COLOR =  'turquoise';//'rgba(3, 29, 52, 0.954)';
 
 // This is the color of array bars that are being compared throughout the animations.
 const SECONDARY_COLOR = 'red';
@@ -29,7 +30,7 @@ export default class SortingVisualizer extends Component {
   resetArray() {
     const array = [];
     for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
-      array.push(randomIntFromIntervals(5, 730));
+      array.push(randomIntFromIntervals(5, 700));
     }
     this.setState({ array });
   }
@@ -46,7 +47,9 @@ export default class SortingVisualizer extends Component {
                 backgroundColor : PRIMARY_COLOR,
               height: `${value}px`,
             }}></div>
+
         ))}
+        <button id="generateArrayButton" onClick={()=>{this.resetArray()}}>Generate New Array</button>
       </div>
     );
   }
