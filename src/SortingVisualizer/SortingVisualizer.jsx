7a122f822c7f53b7e0daaf251130efc67f0ca2c8
Rishav Anand <rishva343@gmail.com>
2022-06-12 13:32:04 +0530

    static random array bars design implemented (not doing anything)

---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import "./SortingVisualizer.css";
+
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 1;
+
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 284;//Screen.width/4-100;
+
+// This is the main color of the array bars.
+const PRIMARY_COLOR = 'turquoise';
+
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = 'red';
+
+export default class SortingVisualizer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      array: [],
+    };
+  }
+
+  componentDidMount() {
+    this.resetArray();
+  }
+
+  resetArray() {
+    const array = [];
+    for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+      array.push(randomIntFromIntervals(5, 730));
+    }
+    this.setState({ array });
+  }
+
+  render() {
+    const { array } = this.state;
+    return (
+        <div className="array-container">
+        {array.map((value, idx) => (
+          <div
+            className="array-bar"
+            key={idx}
+            style={{
+                backgroundColor : PRIMARY_COLOR,
+              height: `${value}px`,
+            }}></div>
+        ))}
+      </div>
+    );
+  }
+}
+
+function randomIntFromIntervals(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
